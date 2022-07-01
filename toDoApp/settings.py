7b9d1006e7aa5e949d55dab8feef247472f3d7a2
ayush2604 <ayush.kr.26.04@gmail.com>
2022-07-01 10:43:09 +0530

    changes

--- toDoApp/settings.py
+++ toDoApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rys4b=!ekg&n7^#2z9pt$a*=grevtcyycp_vxlq3vm2jg!e^i&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','to-do-app-ayush20095023.herokuapp.com']
 
 
 # Application definition
